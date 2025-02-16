f01f5b6a95f7d31e485e5993d58a9151ac049a0f
miguel-kultur <miguel@appkultur.com>
2025-02-16 18:15:11 +0100

    Implement user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User
   field :name, type: String
   field :email, type: String
   field :password, type: String
+
+  validates :name, presence: true, length: {in: 2..100}, format: { with: /\A[a-zA-ZÀ-ÿ'’-]+(?: [a-zA-ZÀ-ÿ'’-]+)*\z/, message: "must be a valid name format"}
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+  validates :password, presence: true, length: { in: 8..24 }, 
+    format: { with: /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,24}\z/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,45 +1,80 @@
 require "test_helper"
 
-class UserTest < Minitest::Test
+class UserTest < ActiveSupport::TestCase
   def setup
     Mongoid.purge! 
-
-    @user = User.create!(name: "John Doe", email: "john@example.com", password: "password123")
+    @email = "john@example.com"
+    @user = User.create!(name: "John Doe", email: @email, password: "Secure@Password#123")
   end
   
-  def test_user_is_valid_with_valid_attributes
+  test "user_should_be_valid_with_valid_attributes" do
     assert @user.valid?
   end
 
-  def test_user_has_name
+  test "user_should_have_name" do
     assert_respond_to @user, :name
     assert_equal "John Doe", @user.name
   end
   
-  # def test_user_requires_name
-  #   @user.name = nil
-  #   assert_not @user.valid?
-  #   assert_includes @user.errors[:name], "can't be blank"
-  # end
+  test "user_should_be_invalid_without_name" do
+    @user.name = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:name], "can't be blank"
+  end
 
-  def test_user_has_email
+  test "user_should_have_email" do
     assert_respond_to @user, :email
     assert_equal "john@example.com", @user.email
   end
 
-  # def test_user_requires_email
-  #   @user.email = nil
-  #   assert_not @user.valid?
-  #   assert_includes @user.errors[:email], "can't be blank"
-  # end
+  test "user_should_be_invalid_without_email" do
+    @user.email = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:email], "can't be blank"
+  end
 
-  def test_user_has_password
+  test "user_should_be_invalid_if_email_is_not_unique" do
+    user = User.new(name: "John Doe", email: "john@example.com", password: "asd#ASD09")
+    assert_not user.valid?, "User should not be valid with non unique email"
+  end  
+
+  test "user_should_have_password" do
     assert_respond_to @user, :password
   end  
 
-  # def test_user_requires_password
-  #   @user.password = nil  
-  #   assert_not @user.valid?
-  #   assert_includes @user.errors[:password], "can't be blank"
-  # end
+  test "user_should_be_invalid_without_password" do
+    @user.password = nil  
+    assert_not @user.valid?
+    assert_includes @user.errors[:password], "can't be blank"
+  end
+
+  test "user_should_be_invalid_with_short_password" do
+    @user.password = "a#A2"
+    assert_not @user.valid?, "User's password should be invalid if shorter than 8 characters"
+  end
+
+  test "user_should_be_invalid_if_password_does_not_have_lower_case_letters" do
+    @user.password = "ASD#@$123"
+    assert_not @user.valid?, "User's password should be invalid without at least one number"
+  end
+
+  test "user_should_be_invalid_if_password_does_not_have_upper_case_letters" do
+    @user.password = "asd#@$123"
+    assert_not @user.valid?, "User's password should be invalid without at least one number"
+  end  
+
+  test "user_should_be_invalid_if_password_does_not_have_numbers" do
+    @user.password = "asd#@$ASD"
+    assert_not @user.valid?, "User's password should be invalid without at least one number"
+  end
+
+  test "user_should_be_invalid_if_password_does_not_have_special_characters" do
+    @user.password = "asdASD123"
+    assert_not @user.valid?, "User's password should be invalid without at least one special character"
+  end  
+
+  test "user_should_be_valid_if_password_is_correct" do
+    @user.password = "asd@ASD09"
+    assert @user.valid?, "User should be valid with correct password"
+  end
 end
